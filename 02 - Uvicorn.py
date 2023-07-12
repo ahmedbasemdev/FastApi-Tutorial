748e4e21471f9bf7af96f23a664d9795bd00c4ae
ahmedbasemdev <ahmedbasem.dev@gmail.com>
2023-07-12 15:27:02 +0300

    base

---
+++ 02 - Uvicorn.py
@@ -0,0 +1,2 @@
+# Unlike the Flask framework, FastAPI doesn’t contain any built-in development server. Hence we need Uvicorn.
+# It implements ASGI standards and is lightning fast. ASGI stands for Asynchronous Server Gateway Interface.
